5d8831d461de55d8dc4c7cb5f14d27ae2d9b8d44
ALuier Bondar <etigerstudio@gmail.com>
2018-06-03 21:23:12 +0800

    Clean comments to maintain readability.

--- MapRouter/GameScene.swift
+++ MapRouter/GameScene.swift
@@ -53,12 +53,8 @@ class GameScene: SKScene,NSGestureRecognizerDelegate {
     private let BUILD_NODES_TEXT = "正在生成节点👷"
     private let RENDER_NODES_TEXT = "正在渲染节点👷"
     private let COMPLETE_TEXT = "完成😘"
-    //private var cachers = [SKEffectNode]()
-    //private let textureWidth = 8192
     
     override func didMove(to view: SKView) {
-        //self.view!.window?.acceptsMouseMovedEvents = true
-        //print(isUserInteractionEnabled)
         loadAsync()
         
         let cameraNode = SKCameraNode()
@@ -107,34 +103,6 @@ class GameScene: SKScene,NSGestureRecognizerDelegate {
             borderNodes[i].isHidden = true
             addChild(borderNodes[i])
         }
-        
-        //calcShortestPath(between: nodes[0], and: nodes[4])
-        //PathFinder.test()
-        
-        // Get label node from scene and store it for use later
-//        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-//        if let label = self.label {
-//            label.alpha = 0.0
-//            label.run(SKAction.fadeIn(withDuration: 2.0))
-//        }
-//
-//        // Create shape node to use during mouse interaction
-//        let w = (self.size.width + self.size.height) * 0.05
-//        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-//
-//        if let spinnyNode = self.spinnyNode {
-//            spinnyNode.lineWidth = 2.5
-//
-//            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-//            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-//                                              SKAction.fadeOut(withDuration: 0.5),
-//                                              SKAction.removeFromParent()]))
-//        }
-//
-//        for i in 0...10{
-//            let multiLineNode = MultiLineNode(n: 1000, x: CGFloat(i))
-//            self.addChild(multiLineNode)
-//        }
     }
     
     func fetchData(){
@@ -156,10 +124,6 @@ class GameScene: SKScene,NSGestureRecognizerDelegate {
                     for _ in 0..<nodeCount {
                         nodes.append(DotNode(index: nextInt(), x: nextInt(), y: nextInt()))
                     }
-//                    print(nodes.reduce((0,0,10000,10000)) { (r:(Int,Int,Int,Int), n) -> (Int,Int,Int,Int) in
-//                        (r.0>n.x ? r.0 : n.x, r.1<n.x ? r.1 : n.x,r.2>n.y ? r.2 : n.y,r.3<n.y ? r.3 : n.y)
-//                    })
-                    //edges.reserveCapacity(edgeCount)
                     for _ in 0..<edgeCount {
                         let node1 = nodes[nextInt()]
                         let node2 = nodes[nextInt()]
@@ -272,23 +236,6 @@ class GameScene: SKScene,NSGestureRecognizerDelegate {
             //addChild(node)
         }
         addChild(dotNode)
-//        cachers.append(SKEffectNode())
-//        cachers.append(SKEffectNode())
-//        cachers[0].shouldRasterize = true
-//        cachers[1].shouldRasterize = true
-//        cachers[0].position.x = -10000
-//        for i in 0..<count{
-//            let node = DotNode(imageNamed: "Node/Default")
-//            node.id = i
-//            node.position = getNodePostion(i)
-//            if i%2==1 {
-//                cachers[0].addChild(node)
-//            } else {
-//                cachers[1].addChild(node)
-//            }
-//        }
-//        addChild(cachers[0])
-//        addChild(cachers[1])
     }
     
     func getNodePostion(_ i:Int) -> CGPoint{
@@ -299,12 +246,9 @@ class GameScene: SKScene,NSGestureRecognizerDelegate {
     func calcShortestPath(between n1:DotNode, and n2:DotNode) -> [DotNode]{
         var result = [DotNode]()
         if let path = PathFinder.shortestPath(source: n1, destination: n2){
-//            var str = "[ "
             for node in path.array {
                 result.append(node as! DotNode)
-//                str += "\((node as! DotNode).index) "
             }
-//            print(str+"]")
         }
         PathFinder.purgeVisited(nodes: nodes)
         return result
@@ -327,96 +271,7 @@ class GameScene: SKScene,NSGestureRecognizerDelegate {
         pathNode.path = path
     }
     
-//    func renderGraph() {
-//        let dotNode = SKSpriteNode()
-//        let buffer = NSImage(size: CGSize(width: textureWidth, height: textureWidth))
-//        let nodeImage = #imageLiteral(resourceName: "Default")
-//        var rect = CGRect(origin: CGPoint.zero, size: nodeImage.size)
-//        let cgBuffer = (nodeImage.cgImage(forProposedRect: &rect, context: nil, hints: nil))!
-//        buffer.lockFocus()
-//        if let context = NSGraphicsContext.current?.cgContext {
-//            for node in nodes{
-//                context.draw(cgBuffer, in: CGRect(origin: CGPoint(x: node.x*8, y: node.y*8-8192), size: rect.size))
-//            }
-//        }
-//        buffer.unlockFocus()
-//        dotNode.texture = SKTexture(image: buffer)
-//        dotNode.size = buffer.size
-//        addChild(dotNode)
-//    }
-    
-//    func renderGraph(count:Int) {
-//        let dotNode = SKNode()
-//        let textureSize = CGSize(width:textureWidth, height:textureWidth)
-//        let lowContext = initCGContext(size: textureSize)
-//        let medContexts = [CGContext](repeating: initCGContext(size: textureSize), count: 4)
-//        let highContexts = [CGContext](repeating: initCGContext(size: textureSize), count: 16)
-////        highContexts.reserveCapacity(4)
-////        for i in 0...3 {
-////            medContexts[i] = initCGContext(size: textureSize)
-////        }
-////        highContexts.reserveCapacity(16)
-////        for i in 0...15 {
-////            highContexts[i] = initCGContext(size: textureSize)
-////        }
-//        let nodeImage = #imageLiteral(resourceName: "Default")
-//        var rect = CGRect(origin: CGPoint.zero, size: nodeImage.size)
-//        var halfSize = CGRect(x: 0, y: 0, width: nodeImage.size.width, height: nodeImage.size.height)
-//        let cgBuffer = (nodeImage.cgImage(forProposedRect: &rect, context: nil, hints: nil))!
-//        let bufferSize = CGSize(width: textureWidth, height: textureWidth)
-//        for i in 0..<count{
-//            let x = nodes[i].x*4, y = nodes[i].y*4
-//            let j = x/textureWidth + (y/textureWidth)*4
-//            highContexts[j].draw(cgBuffer, in: CGRect(origin:
-//                CGPoint(x: x, y: y), size: rect.size))
-//        }
-//        for i in 0...15 {
-//            let partial = SKSpriteNode(texture: SKTexture(cgImage: highContexts[i].makeImage()!))
-//            let x = i%4, y = i/4
-//            partial.position = CGPoint(x: x, y: y)
-//            addChild(partial)
-//        }
-//        print("a")
-//        //dotNode.size = CGSize(width: textureWidth*4, height: textureWidth*4)
-//        //addChild(dotNode)
-//    }
-    
-//    func initCGContext(size: CGSize) -> CGContext! {
-//        let colorSpace = CGColorSpaceCreateDeviceRGB()
-//        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
-//        let context = CGContext(
-//            data: nil,
-//            width: Int(size.width),
-//            height: Int(size.height),
-//            bitsPerComponent: 8,
-//            bytesPerRow: 0,
-//            space: colorSpace,
-//            bitmapInfo: bitmapInfo.rawValue)
-//        return context
-//    }
-    
-//    func drawImageInCGContext(size: CGSize, drawFunc: (_ context: CGContext) -> ()) -> CGImage {
-//        let colorSpace = CGColorSpaceCreateDeviceRGB()
-//        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
-//        let context = CGContext(
-//            data: nil,
-//            width: Int(size.width),
-//            height: Int(size.height),
-//            bitsPerComponent: 8,
-//            bytesPerRow: 0,
-//            space: colorSpace,
-//            bitmapInfo: bitmapInfo.rawValue)!
-//        drawFunc(context)
-//        context.makeImage()
-//        return context.makeImage()!
-//    }
-    
     func touchDown(atPoint pos : CGPoint) {
-        /*if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }*/
         for node in nodes(at: pos) {
             if let dot = node as? DotSpriteNode {
                 let count = selectedNodes.count
@@ -450,17 +305,6 @@ class GameScene: SKScene,NSGestureRecognizerDelegate {
             for node in path {
                 self.dotNode.children[node.index].alpha = 1
             }
-//            for node in self.dotNode.children {
-//                if let dot = node as? DotSpriteNode {
-//                    if path.contains(where: { (e) -> Bool in
-//                        e.index == dot.id
-//                    }) {
-//                        dot.alpha = 0.45
-//                    } else {
-//                        dot.alpha = 1
-//                    }
-//                }
-//            }
             self.updateDistanceBoard(self.calcDistanceBetween(nodes: path))
         }
     }
@@ -505,38 +349,13 @@ class GameScene: SKScene,NSGestureRecognizerDelegate {
             let border = borderNodeFirst ? borderNodes[0] : borderNodes[1]
             border.removeAction(forKey: BORDER_ACTION_KEY)
             border.isHidden = true
-            //borderNodeFirst = !borderNodeFirst
         }
     }
-    
-//    func touchMoved(toPoint pos : CGPoint) {
-//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-//            n.position = pos
-//            n.strokeColor = SKColor.blue
-//            self.addChild(n)
-//        }
-//    }
-//
-//    func touchUp(atPoint pos : CGPoint) {
-//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-//            n.position = pos
-//            n.strokeColor = SKColor.red
-//            self.addChild(n)
-//        }
-//    }
     
     override func mouseDown(with event: NSEvent) {
         self.touchDown(atPoint: event.location(in: self))
     }
-    
-//    override func mouseDragged(with event: NSEvent) {
-//        self.touchMoved(toPoint: event.location(in: self))
-//    }
-//
-//    override func mouseUp(with event: NSEvent) {
-//        self.touchUp(atPoint: event.location(in: self))
-//    }
-//
+
     override func keyDown(with event: NSEvent) {
         switch event.keyCode {
         case 49:
@@ -558,23 +377,11 @@ class GameScene: SKScene,NSGestureRecognizerDelegate {
             } else {
                 connectionNode.isHidden = false
             }
-//            if let label = self.label {
-//                label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-//            }
         case 4:
             connectionNode.isHidden = true
         default: break
-            //print("keyDown: \(event.characters!) keyCode: \(event.keyCode)")
         }
     }
-    
-//    override func update(_ currentTime: TimeInterval) {
-//        if !nodesRendered && nodesLoaded {
-//            renderGraph(count: self.nodes.count)
-//            nodesRendered = true
-//        }
-//        // Called before each frame is rendered
-//    }
     
     @objc func moveCamera(pan: NSPanGestureRecognizer){
         let trans = pan.translation(in: view)
@@ -611,7 +418,6 @@ class GameScene: SKScene,NSGestureRecognizerDelegate {
     
     func setCameraScale(scale: CGFloat) {
         var scale = abs(1/(1+scale) * lastScale)
-        //print(scale)
         if scale<1 {
             scale = 1
         } else if scale > maxScale {
@@ -631,38 +437,6 @@ class DotNode: Node {
         self.y = y
     }
 }
-
-//class LineNode : SKShapeNode {
-//    init(p1:NSPoint, p2:NSPoint){
-//        super.init()
-//        let path:CGMutablePath = CGMutablePath()
-//        path.move(to: p1)
-//        path.addLine(to: p2)
-//        self.path = path
-//        self.strokeColor = .blue
-//    }
-//
-//    required init?(coder aDecoder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//}
-//
-//class MultiLineNode : SKShapeNode {
-//    init(n:Int, x:CGFloat){
-//        super.init()
-//        let path:CGMutablePath = CGMutablePath()
-//        for i in 0...n{
-//            path.move(to: CGPoint(x: x, y: CGFloat(i*2)))
-//            path.addLine(to: CGPoint(x: x, y: CGFloat(i*2+1)))
-//        }
-//        self.path = path
-//        self.strokeColor = .blue
-//    }
-//
-//    required init?(coder aDecoder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//}
 
 class DotSpriteNode: SKSpriteNode {
     var id:Int = 0
